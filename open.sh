2768c9691303ed922ab18d8c92143100e2d1a3f7
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-06-25 13:50:04 +0700

    z

--- open.sh
+++ open.sh
@@ -4,8 +4,8 @@
 ##########################################################################
 ##########################################################################
 ##########################################################################
-                     API="template" 
-                     google_sheet="https://docs.google.com/spreadsheets/d/1H7UgGtAy3JKvulLqGXVm5zeNq1FdNKonMxxBETQtjXQ/edit?gid=1610289956#gid=1610289956" 
+                     API="oauth_bay_68" 
+                     google_sheet="https://docs.google.com/spreadsheets/d/1X23PsL7HwrTKr1YyNAXPSYuTK2-4LmonCPUhVpaJ7lU/edit?gid=1610289956#gid=1610289956" 
                      id="1"                 #เปลี่ยน id ทุกครั้งที่ยิง
                      user="1";            #จำนวนผู้ใช้งาน
                      duration="1";         #วินาที
